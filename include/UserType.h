b99959cf2e894b0a0af21a7f4f0155d1ada02792
Mauricio González <115044134+mau671@users.noreply.github.com>
2024-10-02 18:08:02 -0600

    Delete include/UserType.h

--- include/UserType.h
+++
@@ -1,8 +0,0 @@
-/*
- * Archivo: UserType.h
- * Descripción: Clase que representa un tipo de usuario, especificando una descripción y una prioridad.
- *
- * Autor(es): Mauricio Gonzalez
- */
-
-#pragma once
