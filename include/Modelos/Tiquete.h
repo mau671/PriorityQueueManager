60c4ce99a4d1307ba25fa5c3899f45bae3de7018
Omegames151 <e.meza.1@estudiantec.cr>
2024-10-07 14:34:56 -0600

    tiquete

--- include/Modelos/Tiquete.h
+++ include/Modelos/Tiquete.h
@@ -2,7 +2,7 @@
  * Archivo: Tiquete.h
  * Descripción: Clase que representa los tiquetes a atender.
  *
- * Autor(es): Lun Valverde
+ * Autor(es): Lun Valverde y Josué Meza
  */
 
 #pragma once
@@ -15,6 +15,7 @@
 #include "Estructuras/Concretas/OrderedArrayList.h"
 #include "Modelos/Area.h"
 #include "Modelos/Servicio.h"
+#include "Modelos/TipoUsuario.h"
 
 using std::string;
 
@@ -23,20 +24,34 @@ private:
 	//string codigo;
 	string hora;
 	int prioridad;
+<<<<<<< Updated upstream
 	int nTiquetes=100;//??
 	Servicio* servicio; //agregar clase servicio
 	//Area* area;
+=======
+	Servicio* servicio; //agregar clase servicio
+	Area* area;
+	TipoUsuario* usuario;
+>>>>>>> Stashed changes
 
 public:
 	Tiquete() {
 	}
 
+<<<<<<< Updated upstream
 	Tiquete(string hora, Servicio* servicio) {
 		this->servicio = servicio;
 		this->hora = hora;
 		//area = servicio->getArea();
 		//codigo = area->getCodigo() + std::to_string(nTiquetes);//buscar donde poner un numero global en el proyecto para utilizarlo para los códigos
 		//calcular prioridad
+=======
+	Tiquete(const std::string& hora, Servicio* servicio, TipoUsuario* usuario, int consecutivo)
+		: hora(hora), servicio(servicio), usuario(usuario){
+		this->area = servicio->getArea();
+		this->codigo = area->getCodigo() + std::to_string(consecutivo);//buscar donde poner un numero global en el proyecto para utilizarlo para los códigos
+		this->prioridad = usuario->getPrioridad() * 10 + servicio->getPrioridad();
+>>>>>>> Stashed changes
 	}
 	~Tiquete() {
 		delete servicio;
@@ -50,7 +65,11 @@ public:
 	Servicio* getServicio() {
 		return servicio;
 	}
+<<<<<<< Updated upstream
 	/*
+=======
+
+>>>>>>> Stashed changes
 	Area* getArea() {
 		return area;
 	}
